800f0c6e7b4d9dea7e7de5f0eae57926cf2cbd18
Thomas Hawley <thawley22@turing.edu>
2023-05-26 14:40:52 -0600

    feat: Add a service connection to api endpoint, Add nation instance variable to index action

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,21 @@
 class SearchController < ApplicationController
   def index
-    # require 'pry'; binding.pry
+    @nation_members = find_nation_members(params[:nation])
+    @nation = params[:nation]
+  end
+
+  private
+
+  def find_nation_members(nation)
+    get_url("/api/v1/characters?affiliation=#{nation}&perPage=200")
+  end
+
+  def conn 
+    Faraday.new(url: 'https://last-airbender-api.fly.dev')
+  end
+
+  def get_url(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
   end
 end
